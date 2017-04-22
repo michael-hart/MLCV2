c12d9ef461cf2876a53c24f2fe6b7829d1291df2
Michael Hart <michael.hart1994@gmail.com>
2017-04-22 13:50:06 +0100

    Use of SVD decomposition instead of eigenvectors with least error checking

--- src/q1_transform.m
+++ src/q1_transform.m
@@ -4,30 +4,33 @@
 % Clear variables and figures
 clear;
 clf;
+close all;
 
 % Load previous variables from q1_auto.m; loads imgA, imgB, coordA, coordB
 load('res/auto_out.mat');
 
 % Call the matrix estimation function with coordA, coordB
-transform = estTransformMat2(coordA, coordB);
+transformMat = estTransformMat(coordA, coordB);
 
-% Transform A to B and display alongside B
-% imgAB = imwarp(imgB, transform);
+% Transform B to A and display alongside A
+transform = projective2d(transformMat);
+imgAB = imwarp(imgB, transform);
 
-imgAB = uint8(zeros(size(imgB)));
-for i=1:size(imgB, 1)
-    for j=1:size(imgB, 2)
-        x = j;
-        y = i;
-        homog = transform*[x; y; 1];
-        out = homog ./ homog(3);
-        x2 = round(out(1));
-        y2 = round(out(2));
-        if x2 <= size(imgAB, 2) && y2 <= size(imgAB, 1) && x2 > 0 && y2 > 0
-            imgAB(y2, x2) = imgA(y, x);
-        end
-    end
-end
+% Custom method to transform the image without matlab methods
+% imgAB = uint8(zeros(size(imgB)));
+% for i=1:size(imgB, 1)
+%     for j=1:size(imgB, 2)
+%         x = j;
+%         y = i;
+%         homog = transformMat'*[x; y; 1];
+%         out = homog ./ homog(3);
+%         x2 = round(out(1));
+%         y2 = round(out(2));
+%         if x2 <= size(imgAB, 2) && y2 <= size(imgAB, 1) && x2 > 0 && y2 > 0
+%             imgAB(y2, x2) = imgB(y, x);
+%         end
+%     end
+% end
 
 figure;
 subplot(2, 1, 1);

c50c1bf6afb0a69721b0cf22a8eab98b7f34da22
Meng Kiang Seah <mengkiang92@gmail.com>
2017-04-24 23:28:26 +0100

    Fundamental Matrix estimation function for Q2.2.a.

---
+++ src/estFundamentalMat.m
@@ -0,0 +1,34 @@
+function [ fundamentalMat ] = estFundamentalMat( coordA, coordB )
+%ESTFUNDAMENTALMAT Estimates transformation matrix from A to B
+%   Uses matched co-ordinate pairs in coordA, coordB
+%   coordA and coordB are 2xN matrices, N the same for both
+%   Returns transformMat, a 3x3 transformation matrix
+
+    A = zeros(size(coordA, 2), 9);
+
+    n = 0;
+    for i=1:size(coordA, 2)
+        % Get points from patches
+        xa = coordA(1, i);
+        ya = coordA(2, i);
+        xb = coordB(1, i);
+        yb = coordB(2, i);
+
+        n = n + 1;
+        A(n, :) = [xa * xb, ya * b, xb, ...
+                   xa * yb, ya * yb, yb, ...
+                   xa, ya, 1];
+    end
+
+    ATA = A' * A;
+    % Perform SVD decomposition
+    [~, ~, V] = svd(ATA);
+
+    % Ideal f is : v(1, 9) ... v(9, 9), normalised
+    f = V(:, 9);
+    f = f/f(9);
+
+    fundamentalMat = reshape(f, 3, 3)';
+
+end
+

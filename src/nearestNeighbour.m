2b5832aff9ce7af681db1387844bb2c827e1f29c
Meng Kiang Seah <mengkiang92@gmail.com>
2017-04-25 19:33:02 +0100

    Changed orientation of input/output/

--- src/nearestNeighbour.m
+++ src/nearestNeighbour.m
@@ -1,27 +1,27 @@
 function [ neighbours ] = nearestNeighbour( A, B )
 %NEARESTNEIGHBOUR Returns point in B that is closed to point in A
-%   A and B are n x 2 and m x 2 respectively.
+%   A and B are 2 x n and 2 x m respectively.
 %   Returns neighbours, which is n x 2 of nearest row in B to each row in A
     % Constants
-    n = size(A, 1);
-    m = size(B, 1);
+    n = size(A, 2);
+    m = size(B, 2);
     
     % Output
-    neighbours = zeros(n, 2);
+    neighbours = zeros(2, n);
     
     for index = 1:n
         % Replicate row in A, m times.
-        working = repmat(A(index, :), m, 1);
+        working = repmat(A(:, index), 1, m);
         % So can subtract B. Each row is difference. 
         working = working - B;
         % Square, sum, sqrt, for Euclidean distance.
         working = working .^2;
-        distances = sum(working, 2);
+        distances = sum(working);
         distances = sqrt(distances);
         % Find index of minimum value.
         [~, nearest] = min(distances);
         % Nearest neighbour of point in A is that index in B
-        neighbours(index, :) = B(nearest, :);
+        neighbours(:, index) = B(:, nearest);
     end
 
 end

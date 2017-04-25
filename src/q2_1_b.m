74ae555d98f9372c0f4f8a90c5b81da52482ff46
Meng Kiang Seah <mengkiang92@gmail.com>
2017-04-25 19:37:03 +0100

    Basically done.

--- src/q2_1_b.m
+++ src/q2_1_b.m
@@ -15,21 +15,36 @@ load('auto_out.mat');
 %% Estimate transformation matrices
 transformMatAuto = estTransformMat(coordA, coordB);
 transformMatManual = estTransformMat(manualA, manualB);
+
 %% Warp Images!
+% Transpose required, MATLAB convention
 transformAuto = affine2d(transformMatAuto');
 [imgABAuto, refAuto] = imwarp(imgA, transformAuto);
+
 transformManual = affine2d(transformMatManual');
 [imgABManual, refManual] = imwarp(imgA, transformManual);
 
 %% Display
 figure('position', [0 0 1280 800]);
+
 subplot(2, 2, 1);
 imshow(imgA);
+title('Image A');
+
 subplot(2, 2, 2);
 imshow(imgB);
+title('Image B');
+
 subplot(2, 2, 3);
-imshowpair(imgB, imref2d(size(imgA)), imgABAuto, refAuto);
+imshowpair(imgB, imref2d(size(imgB)), imgABAuto, refAuto);
+title('Image A, Transformed, Auto Features, over B. ');
+
 subplot(2, 2, 4);
-imshowpair(imgB, imref2d(size(imgA)), imgABManual, refManual);
+imshowpair(imgB, imref2d(size(imgB)), imgABManual, refManual);
+title('Image A, Transformed, Manual Features, over B. ');
 
-
+%% Display errors
+errorMan = errorHA(manualA, manualB, transformMatManual);
+errorAuto = errorHA(coordA, coordB, transformMatAuto);
+disp(errorMan);
+disp(errorAuto);

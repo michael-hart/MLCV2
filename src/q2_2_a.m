4a5d278f51a25d37ac335a47f5a3563bee844097
Meng Kiang Seah <mengkiang92@gmail.com>
2017-04-30 00:31:17 +0100

    Images for fundamental, epipolar, stuff.

--- src/q2_2_a.m
+++ src/q2_2_a.m
@@ -3,9 +3,11 @@ clear;
 close all;
 if ismac
     addpath('../res/kitchen');
+    addpath('../res');
     outputpath = ('../');
 else
     addpath('res/kitchen');
+    addpath('../res');
     outputpath = ('');
 end
 
@@ -20,26 +22,39 @@ end
 imgA = imread('FD1.pgm');
 imgB = imread('FD2.pgm');
 
-% figure;
-% p1 = cornerPoints(coordAB_A');
-% p2 = cornerPoints(coordAB_B');
-% showMatchedFeatures(imgA, imgB, p1, p2);
+figure;
+p1 = cornerPoints(coordAB_A');
+p2 = cornerPoints(coordAB_B');
+showMatchedFeatures(imgA, imgB, p1, p2);
 %% Acquire fundamental matrix. Acquire epipole. 
-F = estFundamentalMat(coordAB_A, coordAB_B);
 
-[~, ~, V1] = svd(F);
+F1 = estFundamentalMat(coordAB_A, coordAB_B);
+[~, ~, V1] = svd(F1);
 eA1 = V1(:, end);
 
-F(abs(F) < 0.0001) = 0;
-eA2 = null(F, 'r');
+
+offset = size(imgA)/2+.5;
+offset = fliplr(offset);
+
+N = size(coordAB_A, 2);
+
+coordAB_AFixed = coordAB_A - offset(ones(N, 1), :)';
+coordAB_BFixed = coordAB_B - offset(ones(N, 1), :)';
+
+F2 = estFundamentalMat(coordAB_AFixed, coordAB_BFixed);
+
+[~, ~, V2] = svd(F2);
+eA2 = V2(:, end);
 
 %% Calulate epipolar lines for each point. Plot line. 
 figure;
 imshow([imgA, imgB]);
 hold on;
+scatter(coordAB_A(1, :), coordAB_A(2, :), 50, 'o', 'filled', 'blue');
+
 for index = 1:size(coordAB_A, 2)
-    x1 = coordAB_A(1, index);
-    y1 = coordAB_A(2, index);
+    x1 = coordAB_AFixed(1, index);
+    y1 = coordAB_AFixed(2, index);
     
     x2 = eA2(1);
     y2 = eA2(2);
@@ -47,9 +62,11 @@ for index = 1:size(coordAB_A, 2)
     m = (y2 - y1)/(x2 - x1);
     b = y2 - m * x2;
     
-    xplot = 1:size(imgA, 2);
+    M = size(imgA, 2);
+    xplot = 1:M;
     yplot = m * xplot + b;
-    plot(xplot, yplot);
+    points = round([xplot; yplot] + offset(ones(M, 1), :)');
+    plot(points');
 end
 hold off;
 

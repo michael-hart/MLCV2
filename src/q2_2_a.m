950b1a344f9fb00c8cfa52845c4730a0c6e535bb
Meng Kiang Seah <mengkiang92@gmail.com>
2017-04-29 03:00:54 +0100

    Q2.2.a

--- src/q2_2_a.m
+++ src/q2_2_a.m
@@ -20,15 +20,36 @@ end
 imgA = imread('FD1.pgm');
 imgB = imread('FD2.pgm');
 
-figure;
-p1 = cornerPoints(coordAB_A');
-p2 = cornerPoints(coordAB_B');
-showMatchedFeatures(imgA, imgB, p1, p2);
-%%
+% figure;
+% p1 = cornerPoints(coordAB_A');
+% p2 = cornerPoints(coordAB_B');
+% showMatchedFeatures(imgA, imgB, p1, p2);
+%% Acquire fundamental matrix. Acquire epipole. 
 F = estFundamentalMat(coordAB_A, coordAB_B);
-[~, ~, V] = svd(F' * F);
-ea = V(:, end)
+
+[~, ~, V1] = svd(F);
+eA1 = V1(:, end);
+
 F(abs(F) < 0.0001) = 0;
-eA = null(F, 'r')
-F * ea
-F * eA
+eA2 = null(F, 'r');
+
+%% Calulate epipolar lines for each point. Plot line. 
+figure;
+imshow([imgA, imgB]);
+hold on;
+for index = 1:size(coordAB_A, 2)
+    x1 = coordAB_A(1, index);
+    y1 = coordAB_A(2, index);
+    
+    x2 = eA2(1);
+    y2 = eA2(2);
+    
+    m = (y2 - y1)/(x2 - x1);
+    b = y2 - m * x2;
+    
+    xplot = 1:size(imgA, 2);
+    yplot = m * xplot + b;
+    plot(xplot, yplot);
+end
+hold off;
+

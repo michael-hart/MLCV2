342b442e89472492ac21460b4aa7ecf229ba9d57
Meng Kiang Seah <mengkiang92@gmail.com>
2017-05-01 04:12:19 +0100

    Q.2.2.E done. Pictures included. Data file also included.

---
+++ src/q2_2_e.m
@@ -0,0 +1,82 @@
+%% Question 2 Part 2 Section E
+clear;
+close all;
+if ismac
+    addpath('../res/kitchen');
+    addpath('../res');
+    outputpath = ('../');
+else
+    addpath('res/kitchen');
+    addpath('../res');
+    outputpath = ('');
+end
+
+
+load('q2_2_d.mat');
+imgA = imread('FD1.pgm');
+winSize = 10;
+
+%% Processing
+% Depth, focal length 2mm more.
+figure;
+
+baseline = 20e-2;
+focallength = 26e-3;
+
+depth = 1 ./ disparityX;
+depth = depth * baseline * focallength;
+
+depth(depth == Inf | depth == -Inf) = 0;
+depth = imresize(depth, winSize, 'Method', 'box');
+imshow(depth, [-3e-4, 2e-4]);
+colormap jet;
+colorbar;
+fig = gcf;
+fig.PaperPositionMode = 'auto';
+print([outputpath, 'pic/q2_2_e_focal1'],'-dpng','-r0');
+
+%% Depth, focal length 2mm less.
+
+figure;
+
+baseline = 20e-2;
+focallength = 22e-3;
+
+depth = 1 ./ disparityX;
+depth = depth * baseline * focallength;
+
+depth(depth == Inf | depth == -Inf) = 0;
+depth = imresize(depth, winSize, 'Method', 'box');
+imshow(depth, [-3e-4, 2e-4]);
+colormap jet;
+colorbar;
+fig = gcf;
+fig.PaperPositionMode = 'auto';
+print([outputpath, 'pic/q2_2_e_focal2'],'-dpng','-r0');
+
+%% Depth, Gaussian noise added to Disparity. Zero Mean,
+% No more than 2 pixels... since most lies within 3 std of mean...
+% Standard deviation should be 2/3, let's say 0.5
+% Since Z = (X - mu)/sigma, then X = sigma * Z. (mu = 0)
+
+figure;
+
+baseline = 20e-2;
+focallength = 24e-3;
+
+% Add noise
+disparityXNoise = disparityX + randn(size(disparityX)) * 15;
+
+%
+close all;
+depth = 1 ./ disparityXNoise;
+depth = depth * baseline * focallength;
+
+depth(depth == Inf | depth == -Inf) = 0;
+depth = imresize(depth, winSize, 'Method', 'box');
+imshow(depth, [-3e-4, 2e-4]);
+colormap jet;
+colorbar;
+fig = gcf;
+fig.PaperPositionMode = 'auto';
+print([outputpath, 'pic/q2_2_e_gaussian'],'-dpng','-r0');

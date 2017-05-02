7e0ae86bc1123ff91b9efc95820bd95bf945d4d1
Meng Kiang Seah <mengkiang92@gmail.com>
2017-05-02 11:57:14 +0100

    Changes to naming describe.m.

--- src/describe.m
+++ src/describe.m
@@ -2,33 +2,37 @@ function [ desc ] = describe( img, points )
 %DESCRIBE Describes patches given by 2xN matrix of points
 %   Returns an MxN matrix of descriptors for the points
 
-% Uses each point as the top left point of a 32x32 patch
+% Uses each point as the top (16, 16) point of a 32x32 patch
 % Takes a histogram of each patch and adds to the matrix
 
-% Create output matrix
-N = size(points, 2);
-M = 256;
-desc = zeros(M, N);
+    % Create output matrix
+    N = size(points, 2);
+    M = 64;
+    desc = zeros(M, N);
 
-% Get patch represented by pixel
-% Use min with image dimensions to ensure entire patch
-[height, width] = size(img);
-minHeight = height - 31;
-minWidth = width - 31;
+    % Patch size
+    patchSize = 32;
+    offset = floor(patchSize/2);
+    offset_tau = floor((patchSize - 1)/2);
+    
+    % Pad image
+    img = padarray(img, [offset, offset], 'replicate');
+    % Offset points (1, 1) should become (17, 17);
+    points = points + offset;
+    
+    % Get patch represented by pixel
+    for index=1:N
+        h = points(2, index);
+        w = points(1, index);
+        patch = img(h-offset_tau:h+offset, w-offset_tau:w+offset);
 
-for i=1:N
-    point = points(:, i);
-    h = min(minHeight, point(2));
-    w = min(minWidth, point(1));
-    patch = img(h:h+31, w:w+31);
+        % Get the histogram of the patch
+        descriptor = imhist(patch, M);
 
-    % Get the histogram of the patch
-    descriptor = imhist(patch);
+        % Add the descriptor to the output matrix
+        desc(:, index) = descriptor;
 
-    % Add the descriptor to the output matrix
-    desc(:, i) = descriptor;
-
-end
-
+    end
+    
 end
 
